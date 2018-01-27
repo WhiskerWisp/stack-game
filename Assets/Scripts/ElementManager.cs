3ca15bfb5ef8c26be2a1f0e7c1bbad6646d23fa3
SoraSkyy <viorama@gmail.com>
2018-01-27 15:51:37 +0800

    Added ElementManager script

---
+++ Assets/Scripts/ElementManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementManager : MonoBehaviour {
+
+  public int[] elements;
+  public GameObject elementPrefab;
+  
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < elements.Length; i++) {
+      //print(elements[i]);
+      Instantiate(elementPrefab,
+                  transform.position + (new Vector3(0, i * 0.5f, 0)),
+                  Quaternion.identity);
+    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
