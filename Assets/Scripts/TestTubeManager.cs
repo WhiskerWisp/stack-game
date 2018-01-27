da0251d5076c5d8d9b3f8c2050dc62673e4cf29a
SoraSkyy <viorama@gmail.com>
2018-01-27 16:23:43 +0800

    Added scripts for push and pop buttons

---
+++ Assets/Scripts/TestTubeManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTubeManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  // Called when corresponding PushButton is pressed.
+  public void Push() {
+    
+  }
+
+  // Called when corresponding PopButton is pressed.
+  public void Pop() {
+    
+  }
+  
+}
