1e2a8da7434ebfe32fe9cc10e6a091f5b9bb5062
SoraSkyy <viorama@gmail.com>
2018-01-28 00:21:28 +0800

    Started work on StartGame message-passing script.

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+    public List<int> shelf;
+    public List<int> expected;
+
+    // Use this for initialization
+    void Start () {
+      
+    }
+	
+    // Update is called once per frame
+    void Update () {
+		
+    }
+
+    void OnMouseDown() {
+        DontDestroyOnLoad(gameObject);
+        Application.LoadLevel("MainGame");
+    }
+    
+}
